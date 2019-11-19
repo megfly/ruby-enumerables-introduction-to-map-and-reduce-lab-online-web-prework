eec3bc439d0d2d6a56a001e7f69709f5dc0a6ca8
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-19 18:23:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,3 +14,17 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  new_array = []
+  index = 0 
+  while index < source_array.size do 
+    new_array[index] = source_array[index] *2 
+    index +=1 
+  end 
+  new_array
+end 
+
+def map_to_square(source_array)
+  new_array = [] 
+  index = 0 
+  while index < source_array.size do 
+    new_array[index] = source_array
