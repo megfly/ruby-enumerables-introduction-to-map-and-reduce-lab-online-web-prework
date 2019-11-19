1ea5edf7b663e124af38db781a2b75529e769fe5
candescent-ninja-2792 <mflynn333@gmail.com>
2019-11-19 18:29:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,3 +32,13 @@ def map_to_square(source_array)
   end
   new_array
 end 
+
+def reduce_to_total(source_array, starting_point)
+  total = starting_point
+  index = 0 
+  while index < source_array.size do 
+    total = total + source_array[index]
+    index +=1 
+end
+total 
+end
